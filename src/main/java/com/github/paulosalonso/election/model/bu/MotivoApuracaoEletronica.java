a562b2928863a1171f869746b8b448d30201aca4
Paulo Alonso <paulo_alonso_@hotmail.com>
2022-11-23 19:53:25 -0300

    Includes new constant for enum MotivoApuracaoEletronica

--- src/main/java/com/github/paulosalonso/election/model/bu/MotivoApuracaoEletronica.java
+++ src/main/java/com/github/paulosalonso/election/model/bu/MotivoApuracaoEletronica.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum MotivoApuracaoEletronica implements EnumComCodigo {
     NAO_FOI_POSSIVEL_RECUPERAR_RESULTADO(1),
     URNA_NAO_CHEGOU_MIDIA_DEFEITUOSA(2),
-    URNA_NAO_CHEGOU_MIDIA_EXTRAVIADA(3);
+    URNA_NAO_CHEGOU_MIDIA_EXTRAVIADA(3),
+    OUTROS(99);
 
     private final int codigo;
 }
